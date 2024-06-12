99c92b807e8cbc417f582fd898a5a9da5533040d
Ashilkrishna <ashilkrishna8@gmail.com>
2024-06-12 22:23:35 +0530

    add done

---
+++ add.js
@@ -0,0 +1,19 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+const product=require("./models/shop")
+const {productmodel}=require("./models/shop")
+
+const app=express()
+app.use(cors())
+app.use(express.json())
+mongoose.connect("mongodb+srv://Ashil:Ashil7132@cluster0.js8ogyx.mongodb.net/productDB?retryWrites=true&w=majority&appName=Cluster0")
+app.post("/add",(req,res)=>{
+    let input=req.body
+    let product=new productmodel(input)
+    console.log(product)
+    res.send("product added")
+})
+app.listen(8080,()=>{
+    console.log("server started")
+})
