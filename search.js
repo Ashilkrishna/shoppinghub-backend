e11a8feaaf89000b3fa1ba524a3ddd3a4fbddaef
Ashilkrishna <ashilkrishna8@gmail.com>
2024-06-13 00:57:15 +0530

    search created

--- search.js
+++ search.js
@@ -1,19 +1,25 @@
 const express=require("express")
 const mongoose=require("mongoose")
 const cors=require("cors")
-const product=require("./models/shop")
-const {productmodel}=require("./models/shop")
+const course=require("./models/course")
+const {productmodel}=require("./models/course")
+
 
 const app=express()
 app.use(cors())
 app.use(express.json())
 mongoose.connect("mongodb+srv://Ashil:Ashil7132@cluster0.js8ogyx.mongodb.net/shopDB?retryWrites=true&w=majority&appName=Cluster0")
-app.post("/add",(req,res)=>{
-    let input=req.body
-    let product=new productmodel(input)
-    console.log(product)
-    product.save()
-    res.send("product added")
+app.post("/search",(req,res)=>{
+    let input = req.body
+    productmodel.find(input).then(
+        (data)=>{
+            res.json(data)
+        }
+    ).catch(
+        (error)=>{
+            res.json(error)
+        }
+    )
 })
 app.listen(8080,()=>{
     console.log("server started")
