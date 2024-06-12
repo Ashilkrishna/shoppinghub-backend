c7c8516bc17b2800f89f1f79536e050723b6cec5
Ashilkrishna <ashilkrishna8@gmail.com>
2024-06-12 23:46:18 +0530

    view completed

--- view.js
+++ view.js
@@ -7,12 +7,17 @@ const {productmodel}=require("./models/shop")
 const app=express()
 app.use(cors())
 app.use(express.json())
-mongoose.connect("mongodb+srv://Ashil:Ashil7132@cluster0.js8ogyx.mongodb.net/productDB?retryWrites=true&w=majority&appName=Cluster0")
-app.post("/add",(req,res)=>{
-    let input=req.body
-    let product=new productmodel(input)
-    console.log(product)
-    res.send("product added")
+mongoose.connect("mongodb+srv://Ashil:Ashil7132@cluster0.js8ogyx.mongodb.net/shopDB?retryWrites=true&w=majority&appName=Cluster0")
+app.get("/view",(req,res)=>{
+    productmodel.find().then(
+        (data)=>{
+            res.json(data)
+        }
+    ).catch(
+        (error)=>{
+            res.json(error)
+        }
+    )
 })
 app.listen(8080,()=>{
     console.log("server started")
